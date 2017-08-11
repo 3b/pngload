419636b5a9e05b541c0bc5bcaee43b50545b8801
Michael Fiano <michael.fiano@gmail.com>
2017-08-11 11:14:17 -0400

    More work on deinterlacing.

--- src/deinterlace.lisp
+++ src/deinterlace.lisp
@@ -11,23 +11,51 @@
       (7 7 7 7 7 7 7 7))
   :test #'equalp)
 
+(define-constant +adam7-widths+
+  #(#(0 1 1 1 1 1 1 1 1)
+    #(0 0 0 0 0 1 1 1 1)
+    #(0 1 1 1 1 2 2 2 2)
+    #(0 0 0 1 1 1 1 2 2)
+    #(0 1 1 2 2 3 3 4 4)
+    #(0 0 1 1 2 2 3 3 4)
+    #(0 1 2 3 4 5 6 7 8))
+  :test #'equalp)
+
+(define-constant +adam7-heights+
+  #(#(0 1 1 1 1 1 1 1 1)
+    #(0 1 1 1 1 1 1 1 1)
+    #(0 0 0 0 0 1 1 1 1)
+    #(0 1 1 1 1 2 2 2 2)
+    #(0 0 0 1 1 1 1 2 2)
+    #(0 1 1 2 2 3 3 4 4)
+    #(0 0 1 1 2 2 3 3 4))
+  :test #'equalp)
+
 (defun interlace-method-name (interlace-method)
   (case interlace-method
     (0 :null)
     (1 :adam7)))
 
-(defun make-sub-images (parse-data)
-  (loop :with width = (png-image-width (data parse-data))
-        :with height = (png-image-height (data parse-data))
-        :with sub-images = (make-array 7 :initial-element nil)
-        :for x :from (1- width) :downto 0
-        :do (loop :for y :from (1- height) :downto 0
-                  :do (push (list x y)
-                            (aref sub-images
-                                  (1- (aref +adam7-pattern+
-                                            (mod y 8)
-                                            (mod x 8))))))
-        :finally (return sub-images)))
+(defun calculate-sub-image-dimensions (image-width image-height)
+  (loop :for pass :below 7
+        :collect
+        (flet ((calc (dim array)
+                 (multiple-value-bind (w e) (floor dim 8)
+                   (+ (* (aref (aref array pass) 8) w)
+                      (aref (aref array pass) e)))))
+          (list (calc image-width +adam7-widths+)
+                (calc image-height +adam7-heights+)))))
 
-(defun deinterlace (parse-data)
-  (make-sub-images parse-data))
+(defun add-sub-image (dest pass w h image)
+  (loop :with x1 = (1- (position 1 (aref +adam7-widths+ pass)))
+        :with y1 = (1- (position 1 (aref +adam7-heights+ pass)))
+        :with dx = (/ 8 (aref (aref +adam7-widths+ pass) 8))
+        :with dy = (/ 8 (aref (aref +adam7-heights+ pass) 8))
+        :for sy :below h
+        :for y :from y1 :by dy
+        :do (loop :for sx :below w
+                  :for x :from x1 :by dx
+                  :do (setf (aref dest y x)
+                            (aref image sy sx)))))
+
+(defun deinterlace ())
