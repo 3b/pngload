48148ce78880e3174f86bf3637c451a19698d29d
Michael Fiano <michael.fiano@gmail.com>
2017-08-14 05:37:53 -0400

    Minor changes to tests.

--- src/test.lisp
+++ src/test.lisp
@@ -21,19 +21,18 @@
     (pathname-name file)
     :type (pathname-type file))))
 
-(defun print-image-data (file)
-  (let ((image (ignore-errors (read-png-file file))))
-    (unless (and image
-                 (equalp (mediabox-png::data image)
-                         (opticl:read-image-file file)))
-      (push (get-image-name file) *failed*))))
-
 (defun test-images ()
   (let ((*failed*)
         (files (uiop:directory-files (get-path))))
-    (map nil #'print-image-data files)
-    (format t "Passed (~D)" (- (length files) (length *failed*)))
-    (format t "~&Failed (~D): ~A" (length *failed*) *failed*)))
+    (flet ((test-image (file)
+             (let ((image (ignore-errors (read-png-file file))))
+               (unless (and image
+                            (equalp (mediabox-png::data image)
+                                    (opticl:read-image-file file)))
+                 (push (get-image-name file) *failed*)))))
+      (map nil #'test-image files)
+      (format t "Passed (~D)" (- (length files) (length *failed*)))
+      (format t "~&Failed (~D): ~A" (length *failed*) *failed*))))
 
 (defun test-read-time (library-name func file count)
   (let ((start (local-time:now)))
@@ -47,7 +46,7 @@
 
 (defun test-read-times (file &key (count 1))
   (test-read-time "mediabox-png" #'read-png-file file count)
-  (test-read-time "opticl" #'png-read:read-png-file file count)
+  (test-read-time "opticl" #'opticl:read-image-file file count)
   (test-read-time "cl-png" #'png::decode-file file count)
   (test-read-time "sdl2-image" (lambda (x)
                                  (sdl2:free-surface
