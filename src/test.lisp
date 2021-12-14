5e25f0beadd4ee118098b3e88b77b7c465c25190
Johannes Martinez Calzada <johannes.martinez@gmail.com>
2021-12-13 22:55:59 -0500

    Added testing of pngs loaded from vectors (#3)
    * Add tests for loading from vectors.

--- src/test.lisp
+++ src/test.lisp
@@ -58,67 +58,84 @@
     (:opticl (opticl:read-image-file file))
     (:png-read (load-ref-image/png-read file))))
 
-(defun test-images* (&key flip flatten)
+(defun test-images* (&key flip flatten vector)
   (let ((*failed*)
         (*print-array* nil)
         (files (uiop:directory-files (get-path))))
-    (flet ((test-image (file)
-             (when *verbose*
-               (format t "~% testing ~s flip ~s, flatten ~s~%"
-                       file flip flatten))
-             (multiple-value-bind (image error)
-                 (ignore-errors (load-file file :flip-y flip :flatten flatten))
-               (if error
-                   (progn
-                     (warn "error loading ~s:~%  ~a~%" file error)
-                     (when *break-on-failure*
-                       (break "~s failed~@[: ~s~]" (get-image-name file)
-                              error))
-                     (push (get-image-name file) *failed*))
-                   (multiple-value-bind (ref ref-error)
-                       (ignore-errors (load-ref-image file))
-                     (if ref-error
-                         (progn
-                           (warn "error from reference image loader loading ~s~% ~a~%"
-                                 file ref-error)
-                           (when *break-on-failure*
-                             (break "ref loader failed to load ~s~@[: ~s~]"
-                                    (get-image-name file)
-                                    ref-error))
-                           (push (get-image-name file) *failed*))
-                         (progn
-                           (when flip
-                             (setf ref (opticl:vertical-flip-image ref)))
-                           (when flatten
-                             (setf ref (make-array (array-total-size ref)
-                                                   :element-type (array-element-type ref)
-                                                   :displaced-to ref)))
-                           (unless (and image (equalp (data image) ref))
-                             (when *verbose*
-                               (format t "~s failed ~s~%"
-                                       (get-image-name file)
-                                       (and image (color-type image))))
-                             (when *break-on-failure*
-                               (break "~s failed~@[: ~s~] ~s" (get-image-name file)
-                                      error
-                                      (and image (color-type image))
-                                      image ref))
-                             (push (get-image-name file) *failed*)))))))))
-      (map nil #'test-image files)
-      (format t "~&~%Testing against ~s" *ref*)
-      (when (or flip flatten)
-        (format t " (options: flip ~s, flatten ~s)~%" flip flatten))
-      (format t "~&Passed (~d)" (- (length files) (length *failed*)))
-      (format t "~&Failed (~d)~@[: ~s~]" (length *failed*) *failed*)
-      (list (- (length files) (length *failed*))
-            (length *failed*)))))
+    (macrolet ((common () '(if error
+                            (progn
+                              (warn "error loading ~s:~%  ~a~%" file error)
+                              (when *break-on-failure*
+                                (break "~s failed~@[: ~s~]" (get-image-name file)
+                                       error))
+                              (push (get-image-name file) *failed*))
+                            (multiple-value-bind (ref ref-error)
+                                (ignore-errors (load-ref-image file))
+                              (if ref-error
+                                  (progn
+                                    (warn "error from reference image loader loading ~s~% ~a~%"
+                                          file ref-error)
+                                    (when *break-on-failure*
+                                      (break "ref loader failed to load ~s~@[: ~s~]"
+                                             (get-image-name file)
+                                             ref-error))
+                                    (push (get-image-name file) *failed*))
+                                  (progn
+                                    (when flip
+                                      (setf ref (opticl:vertical-flip-image ref)))
+                                    (when flatten
+                                      (setf ref (make-array (array-total-size ref)
+                                                            :element-type (array-element-type ref)
+                                                            :displaced-to ref)))
+                                    (unless (and image (equalp (data image) ref))
+                                      (when *verbose*
+                                        (format t "~s failed ~s~%"
+                                                (get-image-name file)
+                                                (and image (color-type image))))
+                                      (when *break-on-failure*
+                                        (break "~s failed~@[: ~s~] ~s" (get-image-name file)
+                                               error
+                                               (and image (color-type image))
+                                               image ref))
+                                      (push (get-image-name file) *failed*))))))))
+      (labels ((load-into-vector (file)
+                 (let ((vector (with-open-file (stream file :element-type '(unsigned-byte 8))
+                                 (let ((buffer (make-array (file-length stream) :element-type '(unsigned-byte 8))))
+                                   (read-sequence buffer stream)
+                                   buffer))))
+                   (multiple-value-bind (image error)
+                       (ignore-errors (load-vector vector :flip-y flip :flatten flatten))
+                     (common))))
+               (load-from-file (file)
+                 (multiple-value-bind (image error)
+                     (ignore-errors (load-file file :flip-y flip :flatten flatten))
+                   (common)))
+               (test-image (file)
+                 (when *verbose*
+                   (format t "~% testing ~s flip ~s, flatten ~s, vector ~s~%"
+                           file flip flatten vector))
+                 (if vector
+                     (load-into-vector file)
+                     (load-from-file file))))
+        (map nil #'test-image files)
+        (format t "~&~%Testing against ~s ~@[from vector~^~]" *ref* vector)
+        (when (or flip flatten)
+          (format t " (options: flip ~s, flatten ~s)~%" flip flatten))
+        (format t "~&Passed (~d)" (- (length files) (length *failed*)))
+        (format t "~&Failed (~d)~@[: ~s~]" (length *failed*) *failed*)
+        (list (- (length files) (length *failed*))
+              (length *failed*))))))
 
 (defun test-images (&key (ref :png-read))
   (let* ((*ref* ref)
          (r (list (test-images*)
                   (test-images* :flip t)
                   (test-images* :flatten t)
-                  (test-images* :flip t :flatten t)))
+                  (test-images* :flip t :flatten t)
+                  (test-images* :vector t)
+                  (test-images* :flip t :vector t)
+                  (test-images* :flatten t :vector t)
+                  (test-images* :flip t :flatten t :vector t)))
          (pass (reduce '+ (mapcar #'first r)))
          (fail (reduce '+ (mapcar #'second r))))
     (format t "~%Total: Passed: ~s / Failed: ~s~%" pass fail)
